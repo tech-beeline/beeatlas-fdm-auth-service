f532b98209f2b791c5103c2032924226f76fd814
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-01-29 15:57:20 +0300

    Revert "SFDM-1962 Изменить метод get /user/{login}/info"
    This reverts commit b12d1f56dd94dce4a7299783f804455a5ebf61ad.

--- src/main/java/ru/beeline/fdmauth/service/UserService.java
+++ src/main/java/ru/beeline/fdmauth/service/UserService.java
@@ -65,9 +65,7 @@ public class UserService {
             log.info("userProfile has been created with id=" + userProfile.getId());
         }
         List<ProductDTO> productDTOList = productClient.getProductByUserID(userProfile.getId());
-        if(productDTOList != null) {
-            productDTOList.forEach(productDTO -> productIds.add((long) productDTO.getId()));
-        }
+        productDTOList.forEach(productDTO -> productIds.add((long) productDTO.getId()));
         return getInfo(userProfile, productIds);
     }
 
