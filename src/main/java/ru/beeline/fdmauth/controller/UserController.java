ddd13889efe8af936f3fad38d733a560e193ab3c
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-06-09 13:50:24 +0300

    SFDM-2606 Разработать метод POST /api/v1/user/list

--- src/main/java/ru/beeline/fdmauth/controller/UserController.java
+++ src/main/java/ru/beeline/fdmauth/controller/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
     @GetMapping(value = "/list", produces = "application/json")
     @ResponseBody
     @ApiOperation(value = "Поиск профилей пользователей", response = List.class)
-    public ResponseEntity<List<UserProfileShortDTO>> findUserProfiles(@RequestBody List<Integer> userIds) {
+    public ResponseEntity<List<UserProfileShortDTO>> findUserProfiles(@RequestParam List<Integer> userIds) {
         return ResponseEntity.ok(userProfileService.getUsersByIds(userIds));
     }
 }
